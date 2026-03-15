7c396da46a14d840bfa75402df0e63a9761ef0e4
lichen <39560327+lichen780@users.noreply.github.com>
2026-03-15 16:05:28 +0800

    Add files via upload

---
+++ src/main/java/com/allhook/MainActivity.java
@@ -0,0 +1,14 @@
+package com.allhook;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Xposed 模块已激活", Toast.LENGTH_LONG).show();
+        finish();
+    }
+}
